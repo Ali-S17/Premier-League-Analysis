e6be807da4a5ca21facba5e9c1765eae72735d1f
Ali-S17 <ali13sajad@gmail.com>
2025-08-07 16:27:39 +0100

    Update Code

--- SQL/Home_vs_Away/22_23/Away_Win_Percentage.sql
+++ SQL/Home_vs_Away/22_23/Away_Win_Percentage.sql
@@ -1,1 +1,10 @@
-
+SELECT AwayTeam,
+				COUNT(*) AS Total_Away_Games,
+				SUM(CASE WHEN FTAway > FTHome THEN 1 
+							ELSE 0 END) AS Away_wins,
+				ROUND(SUM(CASE WHEN FTAway > FTHome THEN 1 ELSE 0 END) * 100.0 / COUNT(*) , 2) AS Away_Win_Percentage
+FROM Matches
+			  WHERE Division = 'E0'
+			   AND MatchDate BETWEEN '2022-08-05' AND '2023-05-28'
+GROUP BY AwayTeam
+ORDER BY Away_Win_Percentage DESC;

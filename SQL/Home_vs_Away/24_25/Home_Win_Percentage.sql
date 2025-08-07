26394c142eb912f915c6d0c995e69e9694093968
Ali-S17 <ali13sajad@gmail.com>
2025-08-07 17:25:24 +0100

    Update Code
    Unified multiple spellings of 'Nottingham Forest' to be one row in the GROUP BY function

--- SQL/Home_vs_Away/24_25/Home_Win_Percentage.sql
+++ SQL/Home_vs_Away/24_25/Home_Win_Percentage.sql
@@ -1,1 +1,13 @@
-
+SELECT HomeTeam,
+				CASE WHEN HomeTeam IN ('Nott''m Forest', 'Nottm Forest') THEN 'Nottingham Forest'
+                             ELSE HomeTeam END AS Standardised_HomeTeam,
+				
+				COUNT(*) AS Total_Home_Games,
+				SUM(CASE WHEN FTHome > FTAway THEN 1 
+							ELSE 0 END) AS Home_wins,
+				ROUND(SUM(CASE WHEN FTHome > FTAway THEN 1 ELSE 0 END) * 100.0 / COUNT(*) , 2) AS Home_Win_Percentage
+FROM Matches
+			  WHERE Division = 'E0'
+			   AND MatchDate BETWEEN '2024-08-11' AND '2025-05-28'
+GROUP BY Standardised_HomeTeam
+ORDER BY Home_Win_Percentage DESC;

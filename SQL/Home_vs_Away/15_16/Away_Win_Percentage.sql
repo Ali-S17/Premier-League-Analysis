4a2a3546e5f72e67812c4a2ea27851736cd65abe
Ali-S17 <ali13sajad@gmail.com>
2025-08-07 15:04:29 +0100

    Update Code Away_Win_Percentage.sql

--- SQL/Home_vs_Away/15_16/Away_Win_Percentage.sql
+++ SQL/Home_vs_Away/15_16/Away_Win_Percentage.sql
@@ -1,1 +1,10 @@
-
+SELECT AwayTeam,
+				COUNT(*) AS Total_Away_Games,
+				SUM(CASE WHEN FTAway > FTHome THEN 1 
+							ELSE 0 END) AS away_wins,
+				ROUND(SUM(CASE WHEN FTAway > FTHome THEN 1 ELSE 0 END) * 100.0 / COUNT(*) , 2) AS Away_Win_Percentage
+FROM Matches
+			  WHERE Division = 'E0'
+			   AND MatchDate BETWEEN '2015-08-08' AND '2016-05-17'
+GROUP BY AwayTeam
+ORDER BY Away_Win_Percentage DESC;

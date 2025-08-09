4f83dacd5dddb8768f0c3cd8845bb774713f14b8
Ali-S17 <ali13sajad@gmail.com>
2025-08-09 13:44:15 +0100

    Updated Code

--- SQL/Goals_and_Clean_Sheets/Avg_goals_per_match.sql
+++ SQL/Goals_and_Clean_Sheets/Avg_goals_per_match.sql
@@ -1,1 +1,6 @@
-
+-- Average goals per game by season, query stays the same except MatchDate for each season
+SELECT 
+				ROUND(AVG(FTHome + FTAway), 2)
+FROM Matches
+			  WHERE Division = 'E0'
+		      AND MatchDate BETWEEN '2015-08-01' AND '2016-05-31';

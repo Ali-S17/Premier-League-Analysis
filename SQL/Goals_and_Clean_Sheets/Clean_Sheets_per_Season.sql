5da0b0aa78de5f8de958b5a6e3b1f53186496a8d
Ali-S17 <ali13sajad@gmail.com>
2025-08-09 14:52:36 +0100

    Updated Code

--- SQL/Goals_and_Clean_Sheets/Clean_Sheets_per_Season.sql
+++ SQL/Goals_and_Clean_Sheets/Clean_Sheets_per_Season.sql
@@ -1,1 +1,7 @@
-
+-- Clean Sheets per season, only MatchDate changes in code based on season
+SELECT 
+				SUM(CASE WHEN FTHome = 0 THEN 1 ELSE 0 END) +
+				SUM(CASE WHEN FTAway = 0 THEN 1 ELSE 0 END) AS Total_Clean_Sheets
+FROM Matches
+WHERE Division = 'E0'
+      AND MatchDate BETWEEN '2024-08-01' AND '2025-05-31';

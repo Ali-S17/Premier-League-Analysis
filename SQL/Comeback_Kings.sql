7227e0e77be9fe7dbed8b7ab28a1a6dd8b8eed55
Ali-S17 <ali13sajad@gmail.com>
2025-08-12 16:36:37 +0100

    Updated Code

--- SQL/Comeback_Kings.sql
+++ SQL/Comeback_Kings.sql
@@ -1,2 +1,10 @@
--- How often do teams comeback to win or draw after being behind at half time? Data from 02/03 to 24/25. 
--- Also ranks the teams who have won most games from being behind at half-time.
+-- How often do teams come back to win after being behind at half-time. (From 02/03 & Top 5 Leagues)
+
+SELECT Division, 
+				 SUM(CASE WHEN HTAway > HTHome AND FTHome > FTAway THEN 1 ELSE 0 END) AS Home_Comebacks,
+				 SUM(CASE WHEN HTHome > HTAway AND FTAway > FTHome THEN 1 ELSE 0 END) AS Away_Comebacks,
+				 SUM(CASE WHEN (HTAway > HTHome AND FTHome > FTAway) OR (HTHome > HTAway AND FTAway > FTHome) THEN 1 ELSE 0 END) AS Total_Comebacks
+FROM Matches
+			  WHERE Division IN ('E0', 'F1', 'SP1', 'I1', 'D1')
+			  AND MatchDate  BETWEEN '2002-08-01' AND '2025-05-31'
+GROUP BY Division;
